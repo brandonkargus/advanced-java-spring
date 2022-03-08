db7f44b9444efe3cc91c50c0aa684705d78b3b0c
Brandon <brandonkargus@gmail.com>
2022-03-08 13:36:49 -0600

    HTML, CSS, JS practice

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -2,3 +2,11 @@ function greeting() {
     alert("Hello Spring Developer!!");
 }
 
+function affirmation() {
+    alert("You're understanding it more and more...")
+}
+
+function furtherAffirmation() {
+    alert("AND MORE AND MORE AND MORE AND MORE!!!!!")
+}
+
